d3227c2372db605416ac0039750c123a38d91778
neslihangunduz <128185230+neslihangunduz@users.noreply.github.com>
2024-06-10 14:51:37 +0300

    Update myApp.py

--- Bike/myApp.py
+++ Bike/myApp.py
@@ -155,29 +155,30 @@ Bu veriler, kask kullanımının motosiklet sürücülerinin kazalardan sağ ç�
 
     st.plotly_chart(fig)
 
-    if choice == ("Etkinlik Takvimi"):
+if choice == ("Etkinlik Takvimi"):
         st.title("Hızlı ve Güvenli")
-    def on_button_click():
-        uploaded_file = st.file_uploader("Dosya seç", type=['txt', 'pdf', 'csv'])
-        if uploaded_file is not None:
-            st.write("Dosya yüklendi!")
+    
+        def on_button_click():
+            uploaded_file = st.file_uploader("Dosya seç", type=['txt', 'pdf', 'csv'])
+            if uploaded_file is not None:
+                st.write("Dosya yüklendi!")
 
             #  etkinlik Butonu ekle
 
             # Etkinlik oluştur butonu
-            if st.button("Etkinlik Oluştur"):
-                event_date = st.date_input("Etkinlik Tarihi", date.today())
-                st.write("Seçilen etkinlik tarihi:", event_date)
+                if st.button("Etkinlik Oluştur"):
+                    event_date = st.date_input("Etkinlik Tarihi", date.today())
+                    st.write("Seçilen etkinlik tarihi:", event_date)
 
                 # Seçilen tarihe bağlı olarak açıklama metni
-                if event_date:
-                    event_description_label = f"Açıklama ({event_date.strftime('%d.%m.%Y')})"
-                    event_description = st.text_area(event_description_label)
-                    st.write("Açıklama:", event_description)
+                    if event_date:
+                        event_description_label = f"Açıklama ({event_date.strftime('%d.%m.%Y')})"
+                        event_description = st.text_area(event_description_label)
+                        st.write("Açıklama:", event_description)
 
-                    if st.button("Oluştur"):
-                        event_date = st.date_input("Etkinlik Tarihi", date.today())
-                        st.write("Seçilen etkinlik tarihi:", event_date)
+                        if st.button("Oluştur"):
+                            event_date = st.date_input("Etkinlik Tarihi", date.today())
+                            st.write("Seçilen etkinlik tarihi:", event_date)
 
 
 
